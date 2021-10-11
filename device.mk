7703dccb5b197c400c8b4e5806c8224f31a964a6
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-10-11 12:20:07 +0000

    surya: build vendor.qti.hardware.camera.device@1.0.vendor
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
